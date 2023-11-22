c53e3e33394daf504fc3ed43fc10e28915dbda01
Arthur Birnstiel abirnstiel@icloud.com <abirnstiel@icloud.com>
2023-11-22 18:28:20 +0100

    hid security key

--- api/api/settings.py
+++ api/api/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)fi&6o&pflho7qu%^q$3&dplp0^1n1-cmwaw_((21$60g17cb6'
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -63,7 +63,7 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
 ]
- 
+
 
 ROOT_URLCONF = 'api.urls'
 
